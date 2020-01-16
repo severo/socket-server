95bef624ba8e916a5a7800c02e16271b5417c076
Sylvain Lesage <severo@rednegra.net>
2020-01-17 00:45:24 +0100

    feat: add composition (urlQuerySpec) update

--- index.js
+++ index.js
@@ -15,11 +15,14 @@ const chat = io.on('connection', socket => {
   })
 })
 
+// state
 const guests = new Map()
+let urlQuerySpec = {}
+
 const setGuest = g => guests.set(g.sId, g)
 const deleteGuest = id => guests.delete(id)
 const getGuestsArray = () => [...guests.values()]
-const timestamp = guest => {
+const touch = guest => {
   const t = Date.now()
   setGuest({ ...guest, updatedDate: t, expirationDate: t + 1000 * 60 * 10 })
 }
@@ -28,14 +31,24 @@ const onNewGuest = socket => (guest, ack) => {
   guest.name = guest.name || `Guest_${Date.now()}`
   guest.color = guest.color || rnd({ luminosity: 'dark' })
   setGuest(guest)
-  timestamp(guest)
+  touch(guest)
   emitListGuests()
-  ack(guest)
+  ack(guest, urlQuerySpec)
 }
 const onUpdateGuest = socket => (guest, ack) => {
   // Used to update the name or the color
   // For now, there is no difference with onNewGuest
   onNewGuest(socket)(guest, ack)
+}
+const onUpdateUrlQuerySpec = socket => (newUrlQuerySpec, ack) => {
+  console.log('new urlQuerySpec')
+  const guest = guests.get(socket.id)
+  if (guest !== undefined) {
+    touch(guest)
+    urlQuerySpec = newUrlQuerySpec
+    socket.broadcast.emit('urlqueryspec', urlQuerySpec)
+    ack(urlQuerySpec)
+  }
 }
 const onByeBye = socket => _ => {
   deleteGuest(socket.id)
@@ -55,6 +68,7 @@ const occupappBeta = io.of('/occupapp-beta').on('connection', socket => {
   console.log('New occupapp user connected')
   socket.on('new-guest', onNewGuest(socket))
   socket.on('update-guest', onUpdateGuest(socket))
+  socket.on('update-urlqueryspec', onUpdateUrlQuerySpec(socket))
   socket.on('bye-bye', onByeBye(socket))
 })
 
